8a4f9c8993d05b018bdde82fed64254afe9e2cd5
rongtou <rongtou@gmail.com>
2017-03-03 16:44:01 +0800

    add type specifications

--- src/hash_ring.erl
+++ src/hash_ring.erl
@@ -7,9 +7,20 @@
 -define(VIR_NODE_NUM, 160).
 -define(VIR_NODE_REPS, (?VIR_NODE_NUM div 4)).
 
+% types
+-export_type([ring/0, node_name/0, node_names/0, key/0]).
+
+-type ring() :: gb_trees:tree().
+-type node_name() :: binary().
+-type node_names() :: list(node_name()).
+-type key() :: binary() | string().
+% 
+
+-spec new(node_names()) -> ring().
 new(Nodes) ->
     add_nodes(Nodes, gb_trees:empty()).
 
+-spec add_nodes(node_names(), ring()) -> ring().
 add_nodes(Nodes, Ring) ->
     UpdateFunc = fun(Pos, Node, Ring0) ->
         gb_trees:insert(Pos, Node, Ring0)
@@ -18,6 +29,7 @@ add_nodes(Nodes, Ring) ->
         update_virtual_nodes(Node, Ring0, ?VIR_NODE_REPS, UpdateFunc)
     end,  Ring, Nodes).
 
+-spec remove_node(node_name(), ring()) -> ring().
 remove_node(Node, Ring) ->
     UpdateFunc = fun(Pos, _Node, Ring0) ->
         gb_trees:delete(Pos, Ring0)
@@ -38,6 +50,7 @@ update_ring(<<A,B,C,D,R/binary>>, Node, Ring0, UpdateFunc) ->
     Ring = UpdateFunc(Pos, Node, Ring0),
     update_ring(R, Node, Ring, UpdateFunc).
 
+-spec get_node(key(), ring()) -> node_name().
 get_node(Key, Ring) ->
     HashKey = hash_key(Key),
     Iter = gb_trees:iterator_from(HashKey, Ring),
